1071587d9d200b8d87ab98e97894ca95a4ad2f0e
Merugu Vaishnavi <vaishnavimerugu2005@gmail.com>
2025-11-26 00:21:44 +0530

    Added content CI/CD workflow for Project1

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const PORT = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
 res.writeHead(200, {'Content-Type':'text/plain'});
-res.end('Hello from project1 Docker container!\n');
+res.end('Hello from Project1! CI/CD is working perfectly at ' + new Date().toLocaleTimeString() + '\n');
 	});
 server.listen(PORT, () => console.log('Server running on port${PORT}'));
